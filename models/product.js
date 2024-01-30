e5fc2c3faf7622a07e584ef72dba901312da404d
GerGG <germain.gg90@gmail.com>
2024-01-30 17:13:49 -0400

    models create

---
+++ models/product.js
@@ -0,0 +1,53 @@
+import { createRequire } from "node:module";
+const require = createRequire(import.meta.url);
+const products = require("../products.json");
+
+export class ProductModel {
+  static getAll = async ({ category }) => {
+    if (category)
+      return products.filter(
+        (product) => product.category.toLowerCase() === category.toLowerCase()
+      );
+    return products;
+  };
+
+  static findById = async ({ id }) => {
+    const productById = products.find((product) => product.id === parseInt(id));
+    return productById;
+  };
+
+  static create = async ({ input }) => {
+    const newProduct = {
+      ...input,
+    };
+
+    products.push(newProduct);
+    return newProduct;
+  };
+
+  static findByIndex = async ({ id, input }) => {
+    const productByIndex = products.findIndex(
+      (product) => product.id === parseInt(id)
+    );
+
+    if (productByIndex === -1) return false;
+
+    const productModified = {
+      ...products[productByIndex],
+      ...input,
+    };
+
+    products[productByIndex] = productModified;
+
+    return productModified;
+  };
+
+  static delete = async ({ id }) => {
+    const productByIndex = products.findIndex(
+      (product) => product.id === parseInt(id)
+    );
+    if (productByIndex === -1) return false;
+    products.splice(productByIndex, 1);
+    return true;
+  };
+}
--- routes/products.js
+++ routes/products.js
@@ -1,78 +1,65 @@
 import { Router } from "express";
-import { createRequire } from "node:module";
 import {
   validatePartialProduct,
   validateProduct,
 } from "../schema/productSchema.js";
-
-const require = createRequire(import.meta.url);
-const products = require("../products.json");
+import { ProductModel } from "../models/product.js";
 
 export const productRouter = Router();
 
-productRouter.get("/", (req, res) => {
+productRouter.get("/", async (req, res) => {
+  const { category } = req.query;
+
+  const products = await ProductModel.getAll({ category });
+
   res.json(products);
 });
 
-productRouter.get("/:id", (req, res) => {
+productRouter.get("/:id", async (req, res) => {
   const { id } = req.params;
 
-  const product = products.find((product) => product.id === parseInt(id));
+  const product = await ProductModel.findById({ id });
 
   if (!product) return res.status(404).json({ message: "Product Not Found" });
 
-  res.json(product);
+  res.status(200).json(product);
 });
 
-productRouter.post("/", (req, res) => {
+productRouter.post("/", async (req, res) => {
   const result = validateProduct(req.body);
 
   if (!result.success) {
     return res.status(400).json({ message: JSON.parse(result.error.message) });
   }
 
-  products.push(result.data);
+  const newProduct = await ProductModel.create({ input: result.data });
 
-  res.status(201).json(result.data);
+  res.status(201).json(newProduct);
 });
 
-productRouter.patch("/:id", (req, res) => {
+productRouter.patch("/:id", async (req, res) => {
+  const { id } = req.params;
   const result = validatePartialProduct(req.body);
 
   if (!result.success) {
     return res.status(400).json({ message: JSON.parse(result.error.message) });
   }
 
-  const { id } = req.params;
+  const updateProduct = await ProductModel.findByIndex({
+    id: id,
+    input: result.data,
+  });
 
-  const findIndex = products.findIndex(
-    (product) => product.id === parseInt(id)
-  );
-
-  if (findIndex === -1) {
-    return res.status(400).json({ message: "Product Not Found" });
-  }
-
-  const productModified = {
-    ...products[findIndex],
-    ...result.data,
-  };
-
-  products[findIndex] = productModified;
-  res.status(200).json(productModified);
+  res.status(200).json(updateProduct);
 });
 
-productRouter.delete("/:id", (req, res) => {
+productRouter.delete("/:id", async (req, res) => {
   const { id } = req.params;
 
-  const productId = products.findIndex(
-    (product) => product.id === parseInt(id)
-  );
-  if (productId == -1) {
+  const result = await ProductModel.delete({ id });
+  if (result === false) {
     return res.status(400).json({ message: "Product Not found" });
   }
-
-  products.splice(productId, 1);
 
   res.status(204).send();
 });
